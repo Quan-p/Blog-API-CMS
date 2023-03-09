69786141a6a128b08acd629fb3da345609c4bcf2
quan-p <m.phamquan@gmail.com>
2023-03-09 13:55:11 -0600

    Create login form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,6 +4,15 @@ const LoginPage = () => {
     return (
         <div>
             <h2>Welcome to Login Page</h2>
+            <form>
+                <label>Username:
+                    <input type='text' />
+                </label>
+                <label>Password:
+                    <input type='password' />
+                </label>
+                <input type="submit" value="Submit"></input>
+            </form>
         </div>
     )
 };
