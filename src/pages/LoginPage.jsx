0916852d9eceacb4a118330ff5bb06ff1477378e
quan-p <m.phamquan@gmail.com>
2023-03-09 15:22:18 -0600

    Create updatable state variables for username and password that updates on change

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (event) => {
+
+    }
+
     return (
         <div>
             <h2>Welcome to Login Page</h2>
             <form>
-                <label>Username:
-                    <input type='text' />
+                <label>
+                    Username:
+                    <input type='text' value={username} onChange={(event) => setUsername(event.target.value)} />
                 </label>
-                <label>Password:
-                    <input type='password' />
+                <label>
+                    Password:
+                    <input type='password' value={password} onChange={(event) => setPassword(event.target.value)} />
                 </label>
-                <input type="submit" value="Submit"></input>
+                <button type="submit">Log in</button>
             </form>
         </div>
     )
