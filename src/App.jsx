914e534de7b61767132d3f62c3fd144fda9e49e5
quan-p <m.phamquan@gmail.com>
2023-03-13 14:55:59 -0500

    Import navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import './App.scss'
+import Nav from './components/navbar/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Routes>
         <Route path="/" element={<LoginPage />} />
       </Routes>
