80fcaaa4f7881c2ced393c81503f29c0b2c55539
quan-p <m.phamquan@gmail.com>
2023-03-13 16:09:04 -0500

    Change path for login route

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Nav />
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div>
   )
