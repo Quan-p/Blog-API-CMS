6e0b799804314abafeb0a063ad7525bad77fdfa0
quan-p <m.phamquan@gmail.com>
2023-03-13 16:57:47 -0500

    Create Dashboard page and add route to router

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import './App.scss'
 import Nav from './components/navbar/Nav';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   )
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+const Dashboard = () => {
+    return (
+        <div>This is the dashboard page</div>
+    )
+}
+
+export default Dashboard
