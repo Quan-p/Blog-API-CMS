522f3b5408613da52ba14688cfda7972b66adfbf
quan-p <m.phamquan@gmail.com>
2023-03-14 18:37:58 -0500

    Set the default route on page load and add new_post route

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import LoginPage from './pages/LoginPage';
 import './App.scss'
 import Nav from './components/navbar/Nav';
 import Dashboard from './pages/Dashboard';
+import NewPost from './pages/NewPost';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Routes>
+        <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/new_post" element={<NewPost />} />
       </Routes>
     </div>
   )
