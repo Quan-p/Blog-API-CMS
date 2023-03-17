916db5637f3d96f1e2e62c86fe73b36ff720fa03
quan-p <m.phamquan@gmail.com>
2023-03-17 16:58:33 -0500

    Create a state for username data that can be passed between pages as needed

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,16 @@ import Dashboard from './pages/Dashboard';
 import NewPost from './pages/NewPost';
 
 function App() {
+  const [usernameData, setUsernameData] = useState('');
+
   return (
     <div className="App">
       <Nav />
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/new_post" element={<NewPost />} />
+        <Route path="/login" element={<LoginPage setUsernameData={setUsernameData} />} />
+        <Route path="/dashboard" element={<Dashboard usernameData={usernameData} />} />
+        <Route path="/new_post" element={<NewPost usernameData={usernameData} />} />
       </Routes>
     </div>
   )
