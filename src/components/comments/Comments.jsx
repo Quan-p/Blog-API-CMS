f218e540f892691ec397a1c414a016ef2fee0cad
quan-p <m.phamquan@gmail.com>
2023-03-24 17:22:14 -0500

    Create comments component that renders inside post details

---
+++ src/components/comments/Comments.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+const Comments = ({ postId }) => {
+    const [comments, setComments] = useState();
+  
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const commentsReq = await fetch(`https://blog-api-ifcw.onrender.com/posts/${postId}/comments`);
+                const commentsJson = await commentsReq.json();
+                setComments(commentsJson.comments);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        fetchData();
+    }, []); 
+
+    return (
+        <div>
+            This is the Comments Section
+            {comments ?
+                <div className="comments-container">
+                        {comments.map(comment => {
+                            return <div key={comment._id}>
+                                <h2>{comment.user}</h2>
+                                <p>{comment.date}</p>
+                                <p>{comment.text}</p>
+                            </div>
+                        })}
+                </div> 
+                : <p>Comments loading</p>
+            }
+        </div>
+    )
+}
+
+export default Comments;
