04a09eca578c1ea70dfff1da15206b1147d4ce8e
quan-p <m.phamquan@gmail.com>
2023-03-27 17:17:53 -0500

    Add comment delete button with confirmation based on state

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 
 const Comments = ({ postId }) => {
     const [comments, setComments] = useState();
-  
+    const [confirmDelete, setConfirmDelete] = useState(false);
+
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -26,6 +27,14 @@ const Comments = ({ postId }) => {
                                 <h2>{comment.user}</h2>
                                 <p>{comment.date}</p>
                                 <p>{comment.text}</p>
+                                <button onClick={() => setConfirmDelete(true)}>Delete</button>
+                                    {confirmDelete && (
+                                        <div>
+                                            <p>Are you sure you want to delete?</p>
+                                            <button onClick={handleDelete}>Yes</button>
+                                            <button onClick={() => setConfirmDelete(false)}>No</button>
+                                        </div>
+                                    )}
                             </div>
                         })}
                 </div> 
