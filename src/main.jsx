8797bb861b3e3214327ce90e90b31fc104c7460b
quan-p <m.phamquan@gmail.com>
2023-04-07 16:44:47 -0500

    Replace ReactDOM import and made rootElement more clear

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+
+ReactDOM.createRoot(rootElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<App />
